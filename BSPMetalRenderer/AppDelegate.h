a859b54bfd36aa889e778882d0bceaf41ab2bcf3
Dogeyboy <joaopaixao215@gmail.com>
2025-04-03 12:21:41 -0400

    Initial Commit

---
+++ BSPMetalRenderer/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  BSPMetalRenderer
+//
+//  Created by Joao Da Paixao on 4/3/25.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ BSPMetalRenderer/main.m
@@ -0,0 +1,15 @@
+//
+//  main.m
+//  BSPMetalRenderer
+//
+//  Created by Joao Da Paixao on 4/3/25.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // Setup code that might create autoreleased objects goes here.
+    }
+    return NSApplicationMain(argc, argv);
+}
