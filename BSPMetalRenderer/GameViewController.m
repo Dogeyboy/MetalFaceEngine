557940f02b567329d094de08fdf9217f962ed2a3
Dogeyboy <joaopaixao215@gmail.com>
2025-04-08 10:22:11 -0400

    modified cubeIndices[]

--- BSPMetalRenderer/GameViewController.m
+++ BSPMetalRenderer/GameViewController.m
@@ -172,8 +172,8 @@ static Vertex* flattenBSPTree(BSPNode *node, int *outVertexCount) {
     unsigned int cubeIndices[] = {
         0, 1, 2, 2, 3, 0,  // Front
         4, 5, 6, 6, 7, 4,  // Back
-        0, 3, 7, 7, 4, 0,  // Left
-        1, 2, 6, 6, 5, 1,  // Right
+        0, 4, 7, 7, 3, 0,  // Left
+        1, 5, 6, 6, 2, 1,  // Right
         3, 2, 6, 6, 7, 3,  // Top
         0, 1, 5, 5, 4, 0   // Bottom
     };
